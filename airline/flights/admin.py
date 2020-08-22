c1ca7cc17340323c99eab2c49237396825ea24fe
nishi1234567 <nishishailesh@hotmail.com>
2020-08-22 12:00:10 -0400

    Adding all

---
+++ airline/flights/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Flight,Airport,Passenger
+
+# Register your models here.
+admin.site.register(Flight)
+admin.site.register(Airport)
+admin.site.register(Passenger)
+
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+# Register your models here.
+admin.site.register("", FlightAdmin)
+
